0f88480bbb0be610cf934a6c45b6b7dd7b18db5b
hudacse6 <hudacse6@gmail.com>
2022-01-18 18:31:23 +0600

    add aws assignment 1 description

--- index.php
+++ index.php
@@ -10,4 +10,17 @@ require 'includes/functions.php';
 //Assignment 01 by Zafar
 //Changed by Tanvir_Assignment_01
 
+AWS Assignment 1
+
+1. First create a vpc in Singapore region tagged as like your name.
+2. Create four subnets. Thus each AZ has atleast one Subnets.
+3. 2 Private subnets and 2 Public Subnets.
+4. Change your IAM password.
+5. If you cannot create a new VPC in the Singapore region then create a new VPC in Mumbai/Tokyo/or any other region and do the rest like 1 to 4.
+6. Create one github account
+7. Create one repo with a basic setup
+8. Go to here - https://github.com/devskilldevops/simple-php-website and clone this repo.
+9. Create any feature branch and do some changes (add something)in index.php file and commit and create PR .
+10. and post that link privately to me
+
 init();
